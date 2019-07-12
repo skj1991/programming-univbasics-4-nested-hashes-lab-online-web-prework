c0f6016390b281c0069bcd5da2cc1dd12a927ce9
Stacy Johns <stacykjohns@icloud.com>
2019-07-12 23:06:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
- programmer_hash [:yukihiro_matsumoto] => {
+ programmer_hash [:yukihiro_matsumoto] = {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
