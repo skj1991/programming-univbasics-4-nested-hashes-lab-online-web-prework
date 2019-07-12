fea714070cafa09fb46f36a734dd5ee62d0dd1e7
Stacy Johns <stacykjohns@icloud.com>
2019-07-12 23:25:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -120,6 +120,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly".unshift
+programmer_hash[:dennis_ritchie][:languages] = a.unshift "Assembly"
 return programmer_hash
 end
