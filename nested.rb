e193161235601166fff38b73b83ad0aee43d6d15
Stacy Johns <stacykjohns@icloud.com>
2019-07-12 23:16:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,8 +99,7 @@ def changing_alan
         }
      }
 
-     alans_new_info = "GUI"
-		 programmer_hash[:known_for] = alans_new_info
+		 programmer_hash[:known_for] = "GU!"
 		 return programmer_hash
 
 end
